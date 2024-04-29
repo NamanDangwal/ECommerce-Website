f6790c604ef6ba05142b1359e9c6684fcfd5debd
Naman Dangwa <namandangwal@gmail.com>
2024-04-29 10:40:15 +0530

    .

---
+++ client/src/constant/data.js
@@ -0,0 +1,134 @@
+export const navData = [
+    { url: 'https://img.freepik.com/free-vector/user-rating-feedback-customer-reviews-cartoon-web-icon-e-commerce-online-shopping-internet-buying-trust-metrics-top-rated-product-vector-isolated-concept-metaphor-illustration_335657-2753.jpg?w=2000', text: 'Top Offers' },
+    { url: 'https://static.vecteezy.com/system/resources/previews/005/227/194/original/fruit-on-grocery-bag-cartoon-icon-illustration-food-nature-icon-concept-isolated-premium-flat-cartoon-style-vector.jpg', text: 'Grocery' },
+    { url: 'https://thumbs.dreamstime.com/b/red-phone-vector-illustration-ringing-stationary-old-telephone-rotary-dial-58531578.jpg', text: 'Mobile' },
+    { url: 'https://freedesignfile.com/upload/2019/12/Female_fashion_icons_elegant_cartoon_characters_sketch_vector_design_24754254.jpg', text: 'Fashion' },
+    { url: 'https://cdn-icons-png.flaticon.com/512/4350/4350670.png', text: 'Electronics' },
+    { url: 'https://cdn-icons-png.flaticon.com/512/3884/3884929.png', text: 'Home' },
+    { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1O1I9yTfFkeq0e8XFsW57K9UMbmUFjKVWww&usqp=CAU', text: 'Appliances' },
+    { url: 'https://i.pinimg.com/originals/1f/6e/8b/1f6e8bdc4573d45a06bb78075125fd62.jpg', text: 'Travel' },
+    { url: 'https://w7.pngwing.com/pngs/1023/885/png-transparent-lovely-toy-box-toy-box-plush-toy-bear-toy-car-thumbnail.png', text: 'Beauty, Toys & More' }
+];
+
+export const bannerData = [
+    { id: 1, url: 'https://i7x7p5b7.stackpathcdn.com/codrops/wp-content/uploads/2012/01/image1mpd.png?x94980' },
+    { id: 2, url: 'https://i.gifer.com/Difj.gif' },
+    { id: 3, url: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/695d2794338837.5e7c578df25f0.gif' },
+    { id: 4, url: 'https://fiverr-res.cloudinary.com/t_main1,q_auto,f_auto/gigs2/136060210/original/0d64f4613e000153e0b7efd767c08744eb17bd81.png' }
+]
+
+export const dealData = [
+    { 
+        id: 'product1',
+        url: 'https://rukminim1.flixcart.com/image/200/200/khf63680/cases-covers/back-cover/d/7/g/spigen-acs02256-original-imafxfgbffqaugur.jpeg?q=70', 
+        detailUrl: '',
+        title: {
+            shortTitle: 'Mobile Covers'
+        }, 
+        discount: 'Extra 10% Off', 
+        tagline: 'Deal of the day' 
+    },
+    { 
+        id: 'product2',
+        url: 'https://rukminim1.flixcart.com/image/200/200/k5lcvbk0/moisturizer-cream/9/w/g/600-body-lotion-aloe-hydration-for-normal-skin-nivea-lotion-original-imafz8jb3ftt8gf9.jpeg?q=70', 
+        title: {
+            shortTitle: 'Skin & Hair Care'
+        },
+        discount: 'From 99+5% Off', 
+        tagline: 'Shampoos, Face Washes & More' 
+    },
+    { 
+        id: 'product3',
+        url: 'https://rukminim1.flixcart.com/flap/200/200/image/74bc985c62f19245.jpeg?q=70', 
+        title: {
+            shortTitle: 'Skybags & Safari'
+        }, 
+        discount: 'Upto 70% Off', 
+        tagline: 'Deal of the Day' 
+    },
+    { 
+        id: 'product4',
+        url: 'https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70', 
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        title: {
+            shortTitle: 'Smart Watches',
+            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+        }, 
+        price: {
+            mrp: 6999,
+            cost: 4049,
+            discount: '42%'
+        },
+        description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+        discount: 'Grab Now', 
+        tagline: 'Best Seller' 
+    },
+    { 
+        id: 'product5',
+        url: 'https://rukminim1.flixcart.com/flap/150/150/image/b616a7aa607d3be0.jpg?q=70', 
+        title: {
+            shortTitle: 'Sports & Fitness Essentials'
+        }, 
+        discount: 'Upto 80% Off', 
+        tagline: 'Ab Exerciser, Yoga & more' 
+    }
+];
+
+export const furnitureData = [
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/ke7ff680/hammock-swing/j/f/u/q3-jkaf-y3l0-furniture-kart-original-imafux96kpy7grch.jpeg?q=70', 
+        title: {
+            shortTitle: 'Hammock And Swings'
+        }, 
+        discount: 'From ₹199', 
+        tagline: 'Trendy Collection' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/bean-bag/h/v/b/f8-the-furniture-store-xxxl-original-imae65d3wg7qzpkn.jpeg?q=70', 
+        title: {
+            shortTitle: 'Bean Bag Covers'
+        }, 
+        discount: 'Min 80% Off', 
+        tagline: 'XL, XXL & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jlcmavk0/aquarium-tank/s/4/5/usb-desktop-aquarium-with-running-water-calendar-temperature-and-original-imaf8hv4nkv55gx8.jpeg?q=70', 
+        title: {
+            shortTitle: 'Aquarium Tank'
+        }, 
+        discount: 'From ₹299', 
+        tagline: 'Flat, Round, Cube & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jffpoy80/office-study-chair/v/v/z/pp-am-5001cb-apex-original-imaf3u8rbr5cdycv.jpeg?q=70', 
+        title: {
+            shortTitle: 'Office & Study Chairs'
+        }, 
+        discount: 'Min 50% Off', 
+        tagline: 'Fabric & Leatherette' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jvcp9jk0/recliner/z/w/x/brown-top-grain-leather-sf7018011-1-royaloak-original-imafg9s9hh9vzpf3.jpeg?q=70', 
+        title: {
+            shortTitle: 'Recliner'
+        }, 
+        discount: 'From ₹4999', 
+        tagline: 'Bantia, RoyalOak & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jlqwpe80-1/tv-entertainment-unit/d/t/f/particle-board-holland-tv-unit-black-forzza-black-original-imaf8t5ybywcdtys.jpeg?q=70', 
+        title: {
+            shortTitle: 'Tv Units'
+        }, 
+        discount: 'From ₹2100', 
+        tagline: 'Forzza, Zuari & more' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/inflatable-sofa/6/j/s/wsb031a-velvet-wds-original-imaeaphzbkgrz3xp.jpeg?q=70', 
+        title: {
+            shortTitle: 'Inflatable Sofas'
+        }, 
+        discount: 'Min 50% Off', 
+        tagline: 'Furn Central & more' 
+    },
+];
